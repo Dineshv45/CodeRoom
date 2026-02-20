70cb7ca4e824f6031249fe3cbddeb220be49a64e
Dineshv45 <dineshvattipally45@gmail.com>
2026-02-20 21:48:52 +0530

    update URL for DB

--- server/src/config/connect.js
+++ server/src/config/connect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connection = async () => {
   try {
     await mongoose.connect(
-      process.env.MONGO_URI || "mongodb://127.0.0.1:27017/coderoom"
+      process.env.MONGO_URL || "mongodb://127.0.0.1:27017/coderoom"
     );
 
     console.log(" MongoDB connected");
